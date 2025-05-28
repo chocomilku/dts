5ae19adb703d0cda368361fd455a8021df272477
chocomilku <88021521+chocomilku@users.noreply.github.com>
2025-05-28 15:28:47 +0000

    added create department endpoint

--- server/src/db/models/departments.ts
+++ server/src/db/models/departments.ts
@@ -1,8 +1,13 @@
 import { sql } from "drizzle-orm";
 import { sqliteTable, int, text } from "drizzle-orm/sqlite-core";
+import { z } from "zod";
 
 export const departments = sqliteTable("departments", {
 	id: int().primaryKey({ autoIncrement: true }),
 	name: text().notNull().unique(),
 	createdAt: text().default(sql`(CURRENT_TIMESTAMP)`),
+});
+
+export const zDepartments = z.object({
+	name: z.string(),
 });
--- server/src/routes/departments.route.ts
+++ server/src/routes/departments.route.ts
@@ -1,5 +1,43 @@
+import { db } from "@db/conn";
+import {
+	zDepartments,
+	departments as departmentsModel,
+} from "@db/models/departments";
+import { zValidator } from "@hono/zod-validator";
 import { Hono } from "hono";
+import { SQLiteError } from "bun:sqlite";
 
 const departments = new Hono();
+
+departments.post("/", zValidator("form", zDepartments), async (c) => {
+	const validated = c.req.valid("form");
+
+	try {
+		const insertedData = await db
+			.insert(departmentsModel)
+			.values({
+				name: validated.name,
+			})
+			.returning({ id: departmentsModel.id });
+
+		c.status(201);
+		return c.json({
+			message: "Department Successfully Added!",
+			insertedData,
+		});
+	} catch (e) {
+		if (!(e instanceof SQLiteError)) {
+			console.error(e);
+			c.status(500);
+			return c.json({ message: "Internal Server Error" });
+		}
+
+		// SQLITE_CONSTRAINT_UNIQUE
+		if (e.errno == 2067) {
+			c.status(409);
+			return c.json({ message: "Department name already exists." });
+		}
+	}
+});
 
 export default departments;
