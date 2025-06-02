cadb88fbe7345121791c9a426bfba7fc6ce82026
chocomilku <88021521+chocomilku@users.noreply.github.com>
2025-06-02 16:15:45 +0000

    configured dev environment with docker

--- client/js/constants.js
+++ client/js/constants.js
@@ -1,1 +1,1 @@
-export const API_URL = "http://localhost:54321";
+export const API_URL = "";
--- server/src/db/redis.ts
+++ server/src/db/redis.ts
@@ -2,6 +2,6 @@ import { RedisClient } from "bun";
 
 // TODO: change url info after dockerizing entire app
 // use env passed from docker
-const redis = new RedisClient("redis://localhost:6379");
+const redis = new RedisClient("redis://redis:6379");
 
 export { redis };
