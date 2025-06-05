e46d53c11bda33568c48c0f590dfd50781a21d19
chocomilku <88021521+chocomilku@users.noreply.github.com>
2025-06-05 16:06:57 +0000

    added new document page
    added summernote and jquery for wysiwyg rich text editor

---
+++ client/js/new.page.js
@@ -0,0 +1,11 @@
+// jquery 😭😭😭
+
+// @ts-nocheck
+$(document).ready(function () {
+    $('#summernote').summernote({
+        placeholder: "Write details here",
+        height: 100
+    })
+})
+
+// $('#summernote').summernote('code');
