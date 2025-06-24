158c61e90f88164fe82dc10d35217fe55a279a32
chocomilku <88021521+chocomilku@users.noreply.github.com>
2025-06-24 14:26:35 +0000

    added .env config for mail

--- server/index.ts
+++ server/index.ts
@@ -1,4 +1,14 @@
 import app from "@app";
+import { mailTransporter } from "@mail/mail";
+
+(async () => {
+	await mailTransporter.verify((er, s) => {
+		if (er) {
+			console.error(er);
+			return process.exit(1);
+		}
+	});
+})();
 
 export default {
 	port: 54321,
